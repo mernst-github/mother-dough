e5b52dcf57cd6261d50c3c5c4b151fd855fee5f9
Cloud Mernst <cloud.mernst@gmail.com>
2020-04-27 10:14:58 +0200

    div

--- src/main/java/org/mernst/http/server/Action.java
+++ src/main/java/org/mernst/http/server/Action.java
@@ -1,5 +1,6 @@
 package org.mernst.http.server;
 
+import com.google.common.base.Strings;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import org.mernst.concurrent.Recipe;
@@ -10,6 +11,13 @@ public interface Action {
   static void checkArgument(boolean ok, String errorMsg) {
     if (!ok) {
       throw new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription(errorMsg));
+    }
+  }
+
+  static void checkArgument(boolean ok, String formatString, Object... args) {
+    if (!ok) {
+      throw new StatusRuntimeException(
+          Status.INVALID_ARGUMENT.withDescription(Strings.lenientFormat(formatString, args)));
     }
   }
 }
